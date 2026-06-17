75831c4fa535a8ec174e075a3321230ff628e0c6
mdreh01 <mdreh21@gmail.com>
2026-06-17 17:08:56 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,37 @@
+"use strict"; //treat all JS code as newer version 
+
+// alert(3 + 3) // we are using nodejs, not browser version means we are not at console tab of the google platform
+
+console.log(3 + 3) //code readability should be high
+console.log("Rehan")
+
+console.log(3
+    + 
+     3) //it does not look like this there is no code readability
+
+let name = "Rehan" //String data-type
+let age = 18 //number data-type
+let isLoggedIn = false //boolean data-type
+let state; //this is 'undefined'
+let home = null //here it is assigned as null. null means (khali/empty) we can use to assigne it in the variable.
+
+/*    THIS IS PRIMITIVE DATA-TYPE      */
+
+//number(int) data-type range limit is => 2^53(approx)
+//bigint data-type => large number is known as bigint.
+//string data-type => "" or ''
+//boolean data-type => true or false
+//null => standalone value
+//undefined => (abhi value assigne nhi hui h)
+//symbol => used for uniques data-tye
+
+
+//object
+
+console.log(typeof "Rehan") //output string
+console.log(typeof age) //output number
+console.log(typeof null) //ouput object
+console.log(typeof undefined) //output undefined
+
+//'null' IS A TYPE OF OBJECT WHEN COME AS OUTPUT
+//'undefined' IS A TYPE OF UNDEFINED WHEN COME AS OUTPUT
