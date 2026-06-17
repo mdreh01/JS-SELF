b98eda8b9c08eb355fc80ec804754bc2eac082d9
mdreh01 <mdreh21@gmail.com>
2026-06-17 14:30:44 +0000

    container config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 259802 //'const' sirf hamlog ek ko hi karte h with uniques variable name
+let accountEmail = "mdreh21@gmail.com" //'let' KA HAMLOG BOHOT JYADA USE KARTE H
+var accountPassword = "12345" //'var' ka hamlog use nhi karte
+accountCity = "Jaipur"
+let accountState; //AGAR HAM VARAIBLE DECLARE KARKE USKO CHOD DE BINA KOI VALUE DIYE HOYE TO VO UNDEFINED SHOW HOGA
+
+// accountId = 2 //NOT ALLOWED BECAUSE 'const' SHOULD ALWAYS HAVE ONE VARIABLE WITH SAME NAME SAME VARIABLE NAME SHOULD NOT BE EXECUTED IF WANT TO EXECUTE.
+
+accountEmail = "hc@gmail.com"
+accountPassword = "21212121"
+accountCity = "pune"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]) //OUTPUT WILL GET IN TABULAR FORM.
