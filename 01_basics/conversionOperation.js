0798a40d091fa3407bcebfabb275ed230f5cbef0
mdreh01 <mdreh21@gmail.com>
2026-06-18 18:43:43 +0000

    conversion of datatypes

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -35,3 +35,42 @@ let stringNumber = String(someNumber) //conversion of number into string
 console.log(stringNumber)
 console.log(typeof stringNumber) //output is string
 //output me dikhega number hi but lekin vo backend me string ho chuka h jab ham typeof() karenge tab pata chal jayega
+
+// **************OPERATIONS*************
+
+let value = 3
+let negValue = -value
+console.log(value)
+console.log(negValue)
+console.log(typeof value)
+console.log(typeof negValue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/2)
+console.log(2%2)
+console.log(2**3)
+
+let str1 = "hello"
+let str2 = "Rehan"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + "2") //both are string so easily add ho jayenge lekin mathematical nhi like '12' aise
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+
+console.log(+true) //output  is '1'
+console.log(+"") //output is '0'
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2 //this is not the right way to write a code in this code readbility is dead 
+
+let gameCounter = 100
+++gameCounter //this is increment operator and this is prefix-increment operator
+console.log(gameCounter) //output is 101
+
+// THANK YOU
