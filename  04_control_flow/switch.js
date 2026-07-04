d59d265902148597551e7a20f9b39527a7245a32
mdreh01 <mdreh21@gmail.com>
2026-07-04 18:12:42 +0000

    control flow code files

---
+++  04_control_flow/switch.js
@@ -0,0 +1,22 @@
+// const month = "March";
+
+// switch (month){
+//     case "January":
+//         console.log("1")
+//         break;
+//     case "February":
+//         console.log("2")
+//         break;
+//     case "March":
+//         console.log("3")
+//         break;
+//     case "April":
+//         console.log("4")
+//         break;
+//     case "May":
+//         console.log("5")
+//         break;
+
+//     default:
+//         console.log("Invalid Month")
+// }
