5d71b2417af2ed93a4bb2976630b3655bea0107b
mdreh01 <mdreh21@gmail.com>
2026-07-04 18:17:21 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,48 @@
+// for loop
+
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5) {
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+    
+}
+
+// console.log(element);
+
+for(let i = 1; i <= 10; i++){
+    // console.log(`Outer loop value: ${i}`)
+    for(let j = 1; j <= 10; j++){
+        // console.log(`Outer loop ${j} and Inner loop ${i}`)
+        // console.log(i + '*' + j + ' = ' + i*j)
+        // console.log(`${i} * ${j} = ${i * j}`)
+    }
+} 
+
+let myArray = ["Flash", "Superman", "Batman"]
+// console.log(myArray.length)
+for(let index = 0; index < myArray.length; index++){
+    // console.log(myArray[index])
+}
+
+
+// break and continue
+
+// for(let index = 1; index <= 10; index++){
+//     if(index == 5){
+//         console.log("5 is Detected")
+//         break;
+//     }
+//     console.log(index)
+// }
+
+
+
+for(let index = 1; index <= 10; index++){
+    if(index == 5){
+        console.log("Continue 5")
+        continue;
+    }
+    console.log(index)
+}
